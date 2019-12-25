e677f19069d1742588eba3a0db6f7f9003c2ea68
JosephChataignon <joseph.chataignon@telecom-st-etienne.fr>
2019-12-25 22:22:04 +0100

    genetic algorithm 	modifié :         init.py

--- init.py
+++ init.py
@@ -30,29 +30,120 @@ def doublePoint(nHyperplanes,nDimensions,distrib):
         hyperplanes.append( utils.hyperplaneFromPoints(points) )
     return np.array(hyperplanes)
 
-def poolSelect(nHyperplanes, nDimensions, pCentroids, pMeasure, nConfigs, distrib, m,initType='normal'):
+def poolSelect(nHyperplanes, nDimensions, pCentroids, pMeasure, poolSize, distrib, m,initType='doublePoint'):
     '''
-        Initialize hyperplanes by generating a pool of nConfigs random 
+        Initialize hyperplanes by generating a pool of poolSize random 
         configurations and selecting the one with lowest measure of distortion.
     '''
-    for k in range(nConfigs):
+    for k in range(poolSize):
         if initType == 'normal':
-            hp = normal(nHyperplanes,nDimensions) #can add other initialization functions in elif statements
+            hps = normal(nHyperplanes,nDimensions) 
+        elif initType == 'doublePoint': 
+            hps = doublePoint(nHyperplanes,nDimensions,distrib) 
         else:
             print("ERROR! invalid initialization type")
-        e = ms.measure(m,hp,pCentroids,pMeasure,distrib)
-        if k == 0:
+        e = ms.measure(m,hps,pCentroids,pMeasure,distrib)
+        if k < 1:
             minDistortion = e
-            minHp = hp
+            minConfig = hps
         else:
             if minDistortion >= e:
                 minDistortion = e
-                minHp = hp
-    return minHp
+                minConfig = hps
+    return minConfig
+
+def genetic(nHyperplanes, nDimensions, pCentroids, pMeasure, distrib, m,
+            nConfigs, pGenetic, crossover, mutation, selection='rank', initType='doublePoint'):
+    '''
+        Generates a partially optimized configuration of hyperplanes, with the 
+        goal of having an approximatively equal repartition of the input 
+        distribution over the regions.
+        Here one hyperplane = one gene. At every iteration, one half of the old
+        configs is kept and used to generate the next generation by crossover.
+        -nConfigs is the number of configurations
+        -pGenetic is the number of iterations
+        -crossover is the number of crossing points in the crossover operations
+        -mutation is the mutation method and intensity
+        -selection is the selection method used to chose the configs that are 
+            reproduced
+    '''
+    # Step 1: generating random configurations
+    configs = []
+    measures = []
+    for k in range(nConfigs):
+        if initType == 'normal':
+            config = normal(nHyperplanes,nDimensions) 
+        elif initType == 'doublePoint': 
+            config = doublePoint(nHyperplanes,nDimensions,distrib) 
+        else:
+            print("ERROR! invalid initialization type")
+        configs.append(config)
+        configPerformance = ms.measure(m, config, pCentroids, pMeasure, distrib)
+        measures.append(configPerformance)
+    for k in range(pGenetic):
+        # Step 2: selecting configs to reproduce
+        configs, measures = select(selection, configs, measures)
+        # Step 3: crossing configurations
+        configs = cross(crossover, configs)
+        # Step 4: mutation
+        configs = mutate(mutation, configs)
+    #return
+    #retourner configs
 
 
 
 
+## Genetic algorithm subfunctions
+
+def select(selection, configs, measures):
+    '''
+        Returns the selected configurations that are kept for the next generation.
+    '''
+    n = int(len(configs)/2)
+    if selection == 'rank':
+        s = sorted(zip(configs,measures))
+        configs,measures = map(list, zip(*s))
+        return configs[:n], measures[:n]
+    elif selection == 'random':
+        return configs[:n], measures[:n]
+    else:
+        print('ERROR: unknown selection method')
+
+def cross(crossover, configs, outputSize='default'):
+    '''
+        Crosses the configs 'configs', repeating the operation 'outputSize' times,
+        with 'crossover' crossing points each time.
+    '''
+    newGen = [] # next generation
+    if outputSize == 'default':
+        outputSize = len(configs)
+    for k in range(outputSize):
+        # select 2 configs to cross
+        i,j = np.random.randint(len(configs)),np.random.randint(len(configs))
+        # chose crossing points
+        crosspoints = np.random.randint(len(configs[0]), size=crossover)
+        # cross
+        newConfig = []
+        useI = True # whether to include i or j genes
+        for l in range(len(configs[0])):
+            if useI:
+                newConfig.append(configs[i][l])
+            else:
+                newConfig.append(configs[j][l])
+            if l in crosspoints:
+                useI = not useI
+    return newGen
+
+def mutate(mutation, configs):
+    '''
+        Applies a random mutation to configs. For now, only multiplies every 
+        matrix coefficient with a random normal value.
+    '''
+    newConfigs = []
+    for config in configs:
+        config *= np.random.normal(0,1,np.array(config).shape)
+        newConfigs.append(config)
+    return newConfigs
 
 
 
